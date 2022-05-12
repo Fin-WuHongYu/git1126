ed22929df8ade714398d1d80c575476e81e64fc2
Fin <w13112688556@136.com>
2022-05-12 18:53:35 +0800

    git master from idea by wuhongyu2022-05-12 (01)

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("git test4");
         System.out.println("git test5");
         System.out.println("hot-fix test6");
+        System.out.println("master test6");
+
     }
 }
