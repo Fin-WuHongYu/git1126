bf6c96d41a50279353dc11f95a0ed747ea33e923
Fin <w13112688556@136.com>
2022-05-12 18:51:35 +0800

    git hot-fix from idea by wuhongyu2022-05-12 (01)

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,8 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git test0");
-        System.out.println("git test2");
+        System.out.println("git test10");
+        System.out.println("git test12");
         System.out.println("git test3");
         System.out.println("git test4");
         System.out.println("git test5");
