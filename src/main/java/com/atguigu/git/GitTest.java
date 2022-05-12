5a6b20811e994210728a1800c2b32a57b7c73876
Fin <w13112688556@136.com>
2022-05-12 18:25:19 +0800

    git test3 from idea by wuhongyu2022-05-12 (03)

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,7 +2,10 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git test");
+        System.out.println("git test0");
         System.out.println("git test2");
+        System.out.println("git test3");
+        System.out.println("git test4");
+        System.out.println("git test5");
     }
 }
